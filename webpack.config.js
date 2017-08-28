56067d88ce4c6a77ae18c6a21f365ee89032df7a
Oleksandr Yershov <oleksandr.yershov@itera.no>
2017-08-28 14:58:37 +0300

    feature/authorization_endpoint
    authorization endpoint is created
    login button is enabled

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     rules: [
         {
             test: /\.js$/,
-            exclude: /(node_modules|bower_components)/,
+            exclude: /(node_modules|bower_components|sln)/,
             use: {
                 loader: 'babel-loader',
                 options: {
@@ -19,7 +19,10 @@ module.exports = {
   resolve: {
     modules: ['bower_components', 'node_modules'],
     alias: {
-      registrator: path.resolve(__dirname, 'app/modules')
+      registrator: path.resolve(__dirname, 'app/modules'),
+      views: path.resolve(__dirname, 'app/views'),
+      authentication: path.resolve(__dirname, 'app/modules/authentication'),
+      core: path.resolve(__dirname, 'app/modules/core')
     }
   },
   output: {
